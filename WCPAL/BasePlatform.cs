87ee82ab3c1571ea5724e316e2ccecc4f67e679b
David Mills <david.mills1@sheridanc.on.ca>
2011-06-01 23:55:32 -0400

    updated to initiate and get data from the api server

--- WCPAL/BasePlatform.cs
+++ WCPAL/BasePlatform.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
+using System;
+using System.Xml;
+using System.Runtime.Serialization.Json;
 
 namespace WCPAL
 {
     public class BasePlatform
     {
         string apiUrl = "http://us.battle.net/api/";
+        string controller;
+        string action;
+
+        WebRequest _wr;
 
         internal void InitializeConnection(string url)
         {
+            controller = url;
+        }
 
+        internal XmlDictionaryReader ProcessRequest(string request)
+        {
+            action = request;
+
+            _wr = WebRequest.Create(apiUrl + controller + action);
+
+            HttpWebResponse wres = (HttpWebResponse)_wr.GetResponse();
+
+            XmlDictionaryReader xdr = JsonReaderWriterFactory.CreateJsonReader(wres.GetResponseStream(), new XmlDictionaryReaderQuotas());
+
+            return xdr;
         }
     }
 }

a0d576ed70ee4c1e2f9b1e9b5517b44c87b311de
David Mills <david.mills1@sheridanc.on.ca>
2011-07-12 21:20:24 -0400

    Added model class for the character guild response

---
+++ WCPAL/Model/Guild.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Model
+{
+    class Guild
+    {
+        private string _name;
+        private string _realm;
+        private int _level;
+        private int _members;
+        private int _achPoints;
+        private Emblem _emblem;
+    }
+}

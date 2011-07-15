5ffbd8c4e76e4f48c2c5794dac39eb1ddefb69d2
David Mills <david.mills1@sheridanc.on.ca>
2011-07-15 15:52:05 -0400

    added title model class

---
+++ WCPAL/Model/Title.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Model
+{
+	public class Title
+	{
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+	}
+}

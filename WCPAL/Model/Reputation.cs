6c39412f05f98843d8c0f24076b804899caf5c8a
David Mills <david.mills1@sheridanc.on.ca>
2011-07-15 15:47:23 -0400

    Added reputation model classes

---
+++ WCPAL/Model/Reputation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Model
+{
+    public class Reputation
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        private ReputationStanding _standing;
+
+        public ReputationStanding Standing
+        {
+            get { return _standing; }
+        }
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+        }
+        private int _max;
+
+        public int Max
+        {
+            get { return _max; }
+        }
+    }
+}
---
+++ WCPAL/Model/ReputationStanding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Model
+{
+    public enum ReputationStanding
+    {
+        Hated,
+        Hostile,
+        Unfriendly,
+        Neutral,
+        Friendly,
+        Honored,
+        Revered,
+        Exalted
+    }
+}

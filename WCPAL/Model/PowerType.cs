0d3d95d0cd485f425459c65be2465267dc1c882f
David Mills <david.mills1@sheridanc.on.ca>
2011-07-15 15:19:54 -0400

    Added character stat model classes

---
+++ WCPAL/Model/PowerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Model
+{
+    public enum PowerType
+    {
+        Mana,
+        Rage,
+        Energy,
+        Rune
+    }
+}
---
+++ WCPAL/Model/Stats.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Model
+{
+    public class Stats
+    {
+        private int _health;
+
+        public int Health
+        {
+            get { return _health; }
+            
+        }
+        private PowerType _powerType;
+
+        public PowerType PowerType
+        {
+            get { return _powerType; }
+            
+        }
+        private int _power;
+
+        public int Power
+        {
+            get { return _power; }
+            
+        }
+        private int _str;
+
+        public int Str
+        {
+            get { return _str; }
+            
+        }
+        private int _agi;
+
+        public int Agi
+        {
+            get { return _agi; }
+            
+        }
+        private int _sta;
+
+        public int Sta
+        {
+            get { return _sta; }
+            
+        }
+        private int _int;
+
+        public int Int
+        {
+            get { return _int; }
+            
+        }
+        private int _spr;
+
+        public int Spr
+        {
+            get { return _spr; }
+            
+        }
+        private int _attackPower;
+
+        public int AttackPower
+        {
+            get { return _attackPower; }
+            
+        }
+        private int _rangedAttackPower;
+
+        public int RangedAttackPower
+        {
+            get { return _rangedAttackPower; }
+            
+        }
+        private double _mastery;
+
+        public double Mastery
+        {
+            get { return _mastery; }
+            
+        }
+        private int _masteryRating;
+
+        public int MasteryRating
+        {
+            get { return _masteryRating; }
+            
+        }
+        private double _crit;
+
+        public double Crit
+        {
+            get { return _crit; }
+            
+        }
+        private int _critRating;
+
+        public int CritRating
+        {
+            get { return _critRating; }
+            
+        }
+        private int _hitRating;
+
+        public int HitRating
+        {
+            get { return _hitRating; }
+            
+        }
+        private int _hasteRating;
+
+        public int HasteRating
+        {
+            get { return _hasteRating; }
+            
+        }
+        private int _expertiseRating;
+
+        public int ExpertiseRating
+        {
+            get { return _expertiseRating; }
+            
+        }
+        private int _spellPower;
+
+        public int SpellPower
+        {
+            get { return _spellPower; }
+            
+        }
+        private int _spellPen;
+
+        public int SpellPen
+        {
+            get { return _spellPen; }
+            
+        }
+        private double _spellCrit;
+
+        public double SpellCrit
+        {
+            get { return _spellCrit; }
+            
+        }
+        private int _spellCritRating;
+
+        public int SpellCritRating
+        {
+            get { return _spellCritRating; }
+            
+        }
+        private double _mana5;
+
+        public double Mana5
+        {
+            get { return _mana5; }
+            
+        }
+        private double _mana5Combat;
+
+        public double Mana5Combat
+        {
+            get { return _mana5Combat; }
+            
+        }
+        private int _armor;
+
+        public int Armor
+        {
+            get { return _armor; }
+            
+        }
+        private double _dodge;
+
+        public double Dodge
+        {
+            get { return _dodge; }
+            
+        }
+        private int _dodgeRating;
+
+        public int DodgeRating
+        {
+            get { return _dodgeRating; }
+            
+        }
+        private double _parry;
+
+        public double Parry
+        {
+            get { return _parry; }
+            
+        }
+        private int _parryRating;
+
+        public int ParryRating
+        {
+            get { return _parryRating; }
+            
+        }
+        private double _block;
+
+        public double Block
+        {
+            get { return _block; }
+            
+        }
+        private int _blockRating;
+
+        public int BlockRating
+        {
+            get { return _blockRating; }
+            
+        }
+        private int _resil;
+
+        public int Resil
+        {
+            get { return _resil; }
+            
+        }
+        private double _mainHandDmgMin;
+
+        public double MainHandDmgMin
+        {
+            get { return _mainHandDmgMin; }
+            
+        }
+        private double _mainHandDmgMax;
+
+        public double MainHandDmgMax
+        {
+            get { return _mainHandDmgMax; }
+            
+        }
+        private double _mainHandSpeed;
+
+        public double MainHandSpeed
+        {
+            get { return _mainHandSpeed; }
+            
+        }
+        private double _mainHandDps;
+
+        public double MainHandDps
+        {
+            get { return _mainHandDps; }
+            
+        }
+        private int _mainHandExpertise;
+
+        public int MainHandExpertise
+        {
+            get { return _mainHandExpertise; }
+            
+        }
+        private double _offHandDmgMin;
+
+        public double OffHandDmgMin
+        {
+            get { return _offHandDmgMin; }
+            
+        }
+        private double _offHandDmgMax;
+
+        public double OffHandDmgMax
+        {
+            get { return _offHandDmgMax; }
+            
+        }
+        private double _offHandSpeed;
+
+        public double OffHandSpeed
+        {
+            get { return _offHandSpeed; }
+            
+        }
+        private double _offHandDps;
+
+        public double OffHandDps
+        {
+            get { return _offHandDps; }
+            
+        }
+        private int _offHandExpertise;
+
+        public int OffHandExpertise
+        {
+            get { return _offHandExpertise; }
+            
+        }
+        private double _rangedDmgMin;
+
+        public double RangedDmgMin
+        {
+            get { return _rangedDmgMin; }
+            
+        }
+        private double _rangedDmgMax;
+
+        public double RangedDmgMax
+        {
+            get { return _rangedDmgMax; }
+            
+        }
+        private double _rangedSpeed;
+
+        public double RangedSpeed
+        {
+            get { return _rangedSpeed; }
+            
+        }
+        private double _rangedDps;
+
+        public double RangedDps
+        {
+            get { return _rangedDps; }
+            
+        }
+        private double _rangedCrit;
+
+        public double RangedCrit
+        {
+            get { return _rangedCrit; }
+            
+        }
+        private int _rangedCritRating;
+
+        public int RangedCritRating
+        {
+            get { return _rangedCritRating; }
+            
+        }
+        private int _rangedHitRating;
+
+        public int RangedHitRating
+        {
+            get { return _rangedHitRating; }
+            
+        }
+    }
+}

6f010c53ae09d9a8bd7e5c0aa2a09bb65ac3fe21
David Mills <david.mills1@sheridanc.on.ca>
2011-07-15 22:03:42 -0400

    renamed Raids.cs to Raid.cs

---
+++ WCPAL/Model/Raid.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WCPAL.Model
+{
+    public class Raid
+    {
+        private string _name;
+        private int _normal;
+        private int _heroic;
+        private int _id;
+        private List<Boss> _bosses;
+
+        public Raid(int id, string name, int normal, int heroic, List<Boss> bosses)
+        {
+            _id = id;
+            _name = name;
+            _normal = normal;
+            _heroic = heroic;
+            _bosses = bosses;
+        }
+
+        public static Raid ReadRaids(XElement e)
+        {
+            Raid r;
+
+            List<Boss> b = new List<Boss>();
+
+            foreach (XElement x in e.Elements("bosses"))
+            {
+                b.Add(Boss.ReadBoss(x));
+            }
+
+            r = new Raid(
+                int.Parse(e.Element("id").Value),
+                e.Element("name").Value,
+                int.Parse(e.Element("normal").Value),
+                int.Parse(e.Element("heroic").Value),
+                b
+                );
+
+            return r;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Normal
+        {
+            get { return _normal; }
+        }
+
+        public int Heroic
+        {
+            get { return _heroic; }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public List<Boss> Bosses
+        {
+            get { return _bosses; }
+        }
+
+
+    }
+}

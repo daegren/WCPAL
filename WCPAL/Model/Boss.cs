5d70c646f38eb5f75d07646894e65729d617f87e
David Mills <david.mills1@sheridanc.on.ca>
2011-07-15 22:04:17 -0400

    Updated Progression and Boss classes to read from XElement objects

--- WCPAL/Model/Boss.cs
+++ WCPAL/Model/Boss.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace WCPAL.Model
 {
@@ -11,5 +12,47 @@ namespace WCPAL.Model
         private int _normalKills;
         private int _heroicKills;
         private int _id;
+
+        public Boss(int id, string name, int normalKills, int heroicKills)
+        {
+            _id = id;
+            _name = name;
+            _normalKills = normalKills;
+            _heroicKills = heroicKills;
+        }
+
+        static public Boss ReadBoss(XElement e)
+        {
+            Boss b;
+
+            b = new Boss(
+                int.Parse(e.Element("id").Value),
+                e.Element("name").Value,
+                int.Parse(e.Element("normalKills").Value),
+                int.Parse(e.Element("heroicKills").Value)
+                );
+
+            return b;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public int HeroicKills
+        {
+            get { return _heroicKills; }
+        }
+
+        public int NormalKills
+        {
+            get { return _normalKills; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
     }
 }
--- WCPAL/Model/Progression.cs
+++ WCPAL/Model/Progression.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace WCPAL.Model
 {
     public class Progression
     {
-        private List<Raids> _raids;
+        private List<Raid> _raids;
+
+        public Progression(List<Raid> raids)
+        {
+            _raids = raids;
+        }
+
+        static public Progression ReadProgression(XElement e)
+        {
+            Progression p;
+
+            List<Raid> r = new List<Raid>();
+
+            foreach (XElement x in e.Elements("raids"))
+            {
+                r.Add(Raid.ReadRaids(x));
+            }
+
+            p = new Progression(r);
+
+            return p;
+        }
+
+        public List<Raid> Raids
+        {
+            get { return _raids; }
+        }
     }
 }

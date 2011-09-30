5a17d11f4835adc3564ce6fa0b4425ee882aac5d
David Mills <david.mills1@sheridanc.on.ca>
2011-09-30 15:48:28 -0400

    created static definitions of the classes to match return of API

--- WCPAL/Model/CharacterClass.cs
+++ WCPAL/Model/CharacterClass.cs
@@ -8,17 +8,160 @@ namespace WCPAL
     /// <summary>
     /// The class of a character.
     /// </summary>
-    public enum CharacterClass
+    public class CharacterClass
     {
-        Warrior = 1,
-        Paladin = 2,
-        Hunter = 3,
-        Rogue = 4,
-        Priest = 5,
-        Shaman = 7,
-        Mage = 8,
-        Warlock = 9,
-        Druid = 11,
-        DeathKnight = 16
+        private int _id;
+        private int _mask;
+        private String _name;
+        private PowerType _powerType;
+
+        public int Id { get { return _id; } }
+        public int Mask { get { return _mask; } }
+        public String Name { get { return _name; } }
+        public PowerType PowerType { get { return _powerType; } }
+
+        public CharacterClass()
+        {
+        }
+
+        public static CharacterClass Hunter 
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 3,
+                    _mask = 4,
+                    _name = "Hunter",
+                    _powerType = PowerType.Focus
+                };
+            }
+        }
+
+        public static CharacterClass Rogue
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 4,
+                    _mask = 8,
+                    _name = "Rogue",
+                    _powerType = PowerType.Energy
+                };
+            }
+        }
+
+        public static CharacterClass Warrior
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 1,
+                    _mask = 1,
+                    _name = "Warrior",
+                    _powerType = PowerType.Rage
+                };
+            }
+        }
+
+        public static CharacterClass Paladin
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 2,
+                    _mask = 2,
+                    _name = "Paladin",
+                    _powerType = PowerType.Mana
+                };
+            }
+        }
+
+        public static CharacterClass Shaman
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 7,
+                    _mask = 64,
+                    _name = "Shaman",
+                    _powerType = PowerType.Mana
+                };
+            }
+        }
+
+        public static CharacterClass Mage
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 8,
+                    _mask = 128,
+                    _name = "Mage",
+                    _powerType = PowerType.Mana
+                };
+            }
+        }
+
+        public static CharacterClass Priest
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 5,
+                    _mask = 16,
+                    _name = "Priest",
+                    _powerType = PowerType.Mana
+                };
+            }
+        }
+
+        public static CharacterClass DeathKnight
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 6,
+                    _mask = 32,
+                    _name = "Death Knight",
+                    _powerType = PowerType.Rune
+                };
+            }
+        }
+
+        public static CharacterClass Druid
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 11,
+                    _mask = 1024,
+                    _name = "Druid",
+                    _powerType = PowerType.Mana
+                };
+            }
+        }
+
+        public static CharacterClass Warlock
+        {
+            get
+            {
+                return new CharacterClass()
+                {
+                    _id = 9,
+                    _mask = 256,
+                    _name = "Warlock",
+                    _powerType = PowerType.Mana
+                };
+            }
+        }
     }
 }
--- WCPAL/Model/Race.cs
+++ WCPAL/Model/Race.cs
@@ -8,8 +8,191 @@ namespace WCPAL
     /// <summary>
     /// Character races.
     /// </summary>
-    public enum Race
+    public class Race
     {
+        private int _id;
+        private int _mask;
+        private String _name;
+        private Side _side;
+
+        public int Id { get { return _id; } }
+        public int Mask { get { return _mask; } }
+        public String Name { get { return _name; } }
+        public Side Side { get { return _side; } }
+
+
+        public Race() { }
+
+
+        public static Race Dwarf
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 3,
+                    _mask = 4,
+                    _name = "Dwarf",
+                    _side = Side.Alliance
+                };
+            }
+        }
+
+        public static Race Tauren
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 6,
+                    _mask = 32,
+                    _name = "Tauren",
+                    _side = Side.Horde
+                };
+            }
+        }
+
+        public static Race Undead
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 5,
+                    _mask = 16,
+                    _name = "Undead",
+                    _side = Side.Horde
+                };
+            }
+        }
+
+        public static Race Orc
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 2,
+                    _mask = 2,
+                    _name = "Orc",
+                    _side = Side.Horde
+                };
+            }
+        }
+
+        public static Race Gnome
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 7,
+                    _mask = 64,
+                    _name = "Gnome",
+                    _side = Side.Alliance
+                };
+            }
+        }
+
+        public static Race Troll
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 8,
+                    _mask = 128,
+                    _name = "Troll",
+                    _side = Side.Horde
+                };
+            }
+        }
+
+        public static Race Goblin
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 9,
+                    _mask = 256,
+                    _name = "Goblin",
+                    _side = Side.Horde
+                };
+            }
+        }
+
+        public static Race Draenei
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 11,
+                    _mask = 1024,
+                    _name = "Draenei",
+                    _side = Side.Alliance
+                };
+            }
+        }
+
+        public static Race Worgen
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 22,
+                    _mask = 2097152,
+                    _name = "Worgen",
+                    _side = Side.Alliance
+                };
+            }
+        }
+
+        public static Race BloodElf
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 10,
+                    _mask = 512,
+                    _name = "Blood Elf",
+                    _side = Side.Horde
+                };
+            }
+        }
+
+        public static Race Human
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 1,
+                    _mask = 1,
+                    _name = "Human",
+                    _side = Side.Alliance
+                };
+            }
+        }
+
+        public static Race NightElf
+        {
+            get
+            {
+                return new Race()
+                {
+                    _id = 4,
+                    _mask = 8,
+                    _name = "Night Elf",
+                    _side = Side.Alliance
+                };
+            }
+        }
+
+        /*
         Human = 1,
         Orc = 2,
         Dwarf = 3,
@@ -22,5 +205,12 @@ namespace WCPAL
         BloodElf = 10,
         Draenei = 11,
         Worgen = 22
+        */
+    }
+
+    public enum Side
+    {
+        Alliance,
+        Horde
     }
 }

62dde5b4653a3718cfcd831e725fae55f765a0ee
David Mills <david.mills1@sheridanc.on.ca>
2011-12-27 21:01:29 -0500

    added constants file which contains key locations

---
+++ WCPAL.Tests/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL.Tests
+{
+    public class Constants
+    {
+        public const string PRIVATE_KEY = @"";
+        public const string PUBLIC_KEY = @"";
+    }
+}

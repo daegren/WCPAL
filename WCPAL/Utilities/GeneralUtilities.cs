e4c0fa289fb0a4560d3e41c18b393d7491ea2eeb
David Mills <david.mills1@sheridanc.on.ca>
2011-12-30 00:26:21 -0500

    Added utitlites folder

---
+++ WCPAL/Utilities/GeneralUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCPAL
+{
+    class GeneralUtilites
+    {
+        internal static DateTime ConvertFromUnixTimestamp(string timestamp)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0); //create datetime at UNIX epoch
+            // trim extra 0's off end of string
+            int a = -1;
+            for (int i = timestamp.Length - 1; i > 0; i--)
+            {
+                int n = int.Parse(timestamp[i].ToString());
+                if (n > 0)
+                {
+                    a = i + 1;
+                    break;
+                }
+            }
+            double d = double.Parse(timestamp.Substring(0, a));
+            return origin.AddSeconds(d);
+        }
+    }
+}
